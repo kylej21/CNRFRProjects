a7ddeaf2d2143690d6d5eeeff51a5d38f73b71f1
kylej21 <kylecj21@gmail.com>
2024-06-19 11:06:39 -0700

    Changes made

---
+++ src/app/components/ProgressBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { usePathname } from "next/navigation";
+//@ts-expect-error
+const ProgressBar = ({ progressData }) => {
+    const colors = ['bg-blue-500', 'bg-green-500', 'bg-yellow-500']; // Example colors array
+
+    return (
+        <div className="justify-center items-center flex-col space-y-4 w-1/2">
+            {/*@ts-expect-error*/}
+            {progressData.map((item, index) => (
+                <div className="flex-col text-xl" key={index}>
+                    <div className={`w-full items-center justify-center rounded-full bg-gray-200`}>
+                        <div className={`${colors[index % colors.length]} text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full`} style={{ width: `${item.percentage}%` }}>
+                            {item.percentage}%
+                        </div>
+                    </div>
+                    <div className="text-center mt-2">{item.className}</div>
+                </div>
+            ))}
+        </div>
+    );
+};
+export default ProgressBar;
