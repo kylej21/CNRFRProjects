1363334bc32782bb9374175ef54dac51afff0c07
kylej21 <kylecj21@gmail.com>
2024-06-20 13:03:03 -0700

    added roblox and Kirk CV

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,17 +12,17 @@ export default function Page() {
         <div className="text-center">CodeNinjas Folsom 6/17-6/22</div>
         <div className="text-center text-2xl">For AI camp, Navigate below to see your child&apos;s trained Computer Vision Model</div>
         <div className="justify-center flex space-x-40 text-center">
-          <Link className="bg-white rounded-lg h-40 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Kirk">Kirk</Link>
-          <Link className="bg-white rounded-lg h-40 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Joey">Joey</Link>
-          <Link className="bg-white rounded-lg h-40 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Ethan">Ethan</Link>
+          <Link className="bg-white rounded-lg h-20 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Kirk">Kirk</Link>
+          <Link className="bg-white rounded-lg h-20 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Joey">Joey</Link>
+          <Link className="bg-white rounded-lg h-20 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Ethan">Ethan</Link>
         </div>
         <div className="justify-center flex space-x-40 text-center">
-          <Link className="bg-white rounded-lg h-40 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Evan">Evan</Link>
-          <Link className="bg-white rounded-lg h-40 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Nikan">Nikan</Link>
+          <Link className="bg-white rounded-lg h-20 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Evan">Evan</Link>
+          <Link className="bg-white rounded-lg h-20 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Nikan">Nikan</Link>
         </div>
-        <div className="text-center text-2xl">For Roblox camp, click below to find a link to your childs game:</div>
+        <div className="text-center text-2xl pt-8">For Roblox camp, click below to find a link to your childs game:</div>
         <div className="justify-center flex space-x-40 text-center">
-        <Link className="bg-white rounded-lg h-40 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Roblox">Roblox</Link>
+        <Link className="bg-white rounded-lg h-20 w-60 border-lg border-stone-700 border-2 hover:bg-green-300" href="/Roblox">Roblox</Link>
 
         </div>
       </div>
